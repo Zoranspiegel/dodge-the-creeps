878b2284c06b4f1835a3fb00bea2a2b511525758
Zoranspiegel <zoranbow@gmail.com>
2026-09-06 03:01:57 -0500

    Final touches

--- player.gd
+++ player.gd
@@ -32,11 +32,11 @@ func _process(delta: float) -> void:
 	if velocity.x != 0:
 		$AnimatedSprite2D.animation = "walk"
 		$AnimatedSprite2D.flip_h = velocity.x < 0		
-		$AnimatedSprite2D.flip_v = false
+
 	elif velocity.y != 0:
 		$AnimatedSprite2D.animation = "up"
-		$AnimatedSprite2D.flip_v = velocity.y > 0
 	
+	$AnimatedSprite2D.flip_v = velocity.y > 0
 	
 	position += velocity * delta
 	position = position.clamp(Vector2.ZERO, screen_size)
