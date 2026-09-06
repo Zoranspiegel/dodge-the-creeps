56b5f2367c2002987a0c30a2c41bb3ad37d0dab7
Zoranspiegel <zoranbow@gmail.com>
2026-09-06 02:50:27 -0500

    Music && GameOver sound && StartButton shortcut

--- main.gd
+++ main.gd
@@ -7,6 +7,8 @@ func game_over() -> void:
 	$ScoreTimer.stop()
 	$MobTimer.stop()
 	$HUD.show_game_over()
+	$Music.stop()
+	$DeathSound.play()
 	
 func new_game() -> void:
 	score = 0
@@ -14,7 +16,8 @@ func new_game() -> void:
 	$StartTimer.start()
 	$HUD.update_score(score)
 	$HUD.show_message("Get Ready")
-
+	$Music.play()
+	get_tree().call_group("mobs", "queue_free")
 
 func _on_mob_timer_timeout() -> void:
 	var mob: RigidBody2D = mob_scene.instantiate()
